4c80fa74af11dc79c13d2b11dbb71dccf695c409
Plakad <kanpot.tsch@gmail.com>
2024-10-04 21:55:35 +0700

    follow by GPT 2

--- app.py
+++ app.py
@@ -36,7 +36,8 @@ credentials_info = {
     "auth_uri": "https://accounts.google.com/o/oauth2/auth",
     "token_uri": "https://oauth2.googleapis.com/token",
     "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-    "client_x509_cert_url": os.getenv("client_x509_cert_url")
+    "client_x509_cert_url": os.getenv("client_x509_cert_url"),
+    "universe_domain": "googleapis.com"
 }
 
 # Create Google Sheets credentials object
